d6be7a52fe2722886515102b7021f646b3054e1c
Arnelle Balane <arnellebalane@gmail.com>
2018-06-24 19:28:22 +0800

    Generated project setup using vue cli

---
+++ components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app" class="app">
+        <h1>css-containment-example</h1>
+        <p>CSS Containment Property Demo</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    };
+</script>
---
+++ readme.md
@@ -0,0 +1,10 @@
+# css-containment-example
+
+CSS Containment Property Demo
+
+# Running the project
+
+```bash
+$ npm install
+$ npm start
+```
