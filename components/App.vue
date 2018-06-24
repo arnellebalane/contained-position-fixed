8a800a7891c04c53f3abba772e15b706d0416d76
Arnelle Balane <arnellebalane@gmail.com>
2018-06-24 19:59:59 +0800

    Add template for container and fixed elements

--- components/App.vue
+++ components/App.vue
@@ -1,7 +1,17 @@
 <template>
     <div id="app" class="app">
-        <h1>css-containment-example</h1>
-        <p>CSS Containment Property Demo</p>
+        <header>
+            <h1>CSS Containment Example</h1>
+            <a href="https://github.com/arnellebalane/css-containment-example" target="_blank" rel="noopener noreferrer">
+                github.com/arnellebalane/css-containment-example
+            </a>
+        </header>
+
+        <main>
+            <div class="container">
+                <div class="element"></div>
+            </div>
+        </main>
     </div>
 </template>
 
@@ -10,3 +20,41 @@
         name: 'App'
     };
 </script>
+
+<style scoped>
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    header {
+        flex-shrink: 0;
+        padding: 52px 10vmin;
+    }
+
+    header * {
+        margin: 0;
+    }
+
+    main {
+        flex-grow: 1;
+        display: flex;
+        padding: 10vmin;
+        padding-top: 0;
+    }
+
+    .container {
+        width: 100%;
+        background-color: #3F51B5;
+    }
+
+    .element {
+        width: 50vw;
+        height: 50vh;
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        background-color: #E91E63;
+    }
+</style>
